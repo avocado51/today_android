ac13fbf76e0584ad52454719d503d5c66eeae2ca
shinee20 <ujmikzz7@hanmail.net>
2018-12-15 19:05:52 +0900

    Update CheerupMsgData.kt

--- app/src/main/java/com/kb/challenge/app/today/today_android/model/cheerup/CheerupMsgData.kt
+++ app/src/main/java/com/kb/challenge/app/today/today_android/model/cheerup/CheerupMsgData.kt
@@ -6,11 +6,7 @@ import java.util.*
  * Created by shineeseo on 2018. 11. 11..
  */
 data class CheerupMsgData (
-<<<<<<< HEAD
         var today_at : String,
-=======
-        var feeling_at : String,
->>>>>>> 591354f86d759d4fc80165b174d14ce49bdee29e
         var good : Int,
         var comment : String
 )
